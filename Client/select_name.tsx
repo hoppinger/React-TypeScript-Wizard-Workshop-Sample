58de9f8220b41d06d1550c7ad7390dab9f3766b9
giuseppemag <giuseppemag@gmail.com>
2017-06-22 10:48:40 +0200

    Setup empty starting point.

--- Client/select_name.tsx
+++ Client/select_name.tsx
@@ -1,22 +1,3 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import * as Immutable from "immutable"
-
-export type SelectNameProps = { value:string, setValue:(new_name:string,callback?:()=>void)=>void, moveNext:() => void }
-export type SelectNameState = { }
-export class SelectName extends React.Component<SelectNameProps, SelectNameState> {
-  constructor(props:SelectNameProps, context:any) {
-    super(props, context)
-    this.state = {}
-  }
-
-  render() : JSX.Element {
-    return <div>
-             <input type="text"
-                    value={this.props.value}
-                    onChange={e => this.props.setValue(e.currentTarget.value)}
-                   />
-             <button disabled={this.props.value.length <= 0} onClick={() => this.props.moveNext()}>Next</button>
-           </div>
-  }
-}
--- Client/select_number.tsx
+++ Client/select_number.tsx
@@ -2,32 +2,3 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import * as Immutable from "immutable"
 import * as Api from './api'
-
-export type SelectNumberProps = { value:number, setValue:(new_value:number,callback?:()=>void)=>void, moveNext:() => void }
-export type SelectNumberState = { available_numbers:"loading"|Immutable.List<number>, selection:undefined|number }
-export class SelectNumber extends React.Component<SelectNumberProps, SelectNumberState> {
-  constructor(props:SelectNumberProps, context:any) {
-    super(props, context)
-    this.state = { available_numbers:"loading", selection:undefined }
-  }
-
-  componentWillMount() {
-    Api.getAvailableNumbers().then(n => this.setState({...this.state, available_numbers: n}))
-  }
-
-  render() : JSX.Element {
-    return this.state.available_numbers == "loading" ? <div>Loading...</div>
-          :
-        <div>
-          <select value={this.props.value}
-                  onChange={e => this.props.setValue(parseInt(e.currentTarget.value))} >
-             <option value=""></option>
-             {
-               this.state.available_numbers.map(i => <option value={i}>{i}</option>)
-             }
-           </select>
-          <button disabled={!this.state.available_numbers.some(n => n == this.props.value)}
-                  onClick={() => this.props.moveNext()}>Next</button>
-        </div>
- }
-}
